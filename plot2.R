5f248961e349aa5df29ef9f42c8a4956ad3d3428
traley <timraley444@gmail.com>
2015-09-12 14:46:09 -0400

    add project1 files

---
+++ plot2.R
@@ -0,0 +1,13 @@
+##This script is to create Plot #2 for Exploratory Data Analysis - Project #1
+
+##Read in File
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Create Plot 
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
